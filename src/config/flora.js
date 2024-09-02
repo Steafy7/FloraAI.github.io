c887495c1970f30c5ef60a69e3bd50e1fa394d6b
Steafy7 <eve.leaf77@gmail.com>
2024-09-02 15:20:01 -0400

    fixed api key issue being on the frontend

--- src/config/flora.js
+++ src/config/flora.js
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-    apiKey: "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",
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
     dangerouslyAllowBrowser: true
 });
 

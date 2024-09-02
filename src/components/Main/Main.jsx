7939dc77ed78dd96980643e728992e417e9893be
Steafy7 <eve.leaf77@gmail.com>
2024-09-01 21:04:54 -0400

    Flora AI first commit - loading bars don't work yet

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,74 @@
+import React, { useContext } from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
+
+const Main = () => {
+
+    const {onSent,recentPrompt,showResult,loading,resultData,setInput,input,messages} = useContext(Context)
+
+  return (
+    <div className='main'>
+        <div className="nav">
+            <p>Flora</p>
+            <img src={assets.user_icon} alt="" />
+        </div>
+        <div className="main-container">
+
+            {!showResult
+            ?
+            <>
+                <div className="greet">
+                    <p><span>Hello, Dev.</span></p>
+                    <p>How can I help you today?</p>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                        <p>What is the most trending place to visit in Canada?</p>
+                        <img src={assets.compass_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Briefly summarize this concept: catalyst in chemistry</p>
+                        <img src={assets.bulb_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Brainstorm team bonding activities for a computer science group project</p>
+                        <img src={assets.message_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Improve the readability of the following code</p>
+                        <img src={assets.code_icon} alt="" />
+                    </div>
+                </div>
+                </>
+                :<div className='result'>
+                    {messages.map((message, i) =>
+                        <div key={i} className={!message.isBot?"result-title":"result-data"}>
+                            <img src={!message.isBot?assets.user_icon:assets.flora_icon} alt='' />
+                            {message.isBot? <p dangerouslySetInnerHTML={{ __html: i === messages.length - 1 ? resultData : message.text }}></p> : <p>{message.text}</p>}
+                        </div>
+                    )}
+                </div>
+            }
+
+            
+
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input onChange={(e)=>setInput(e.target.value)} value={input} type='text' placeholder='Enter a prompt here'/>
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img onClick={()=>onSent()} src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+                <p className='bottom-info'>
+                Flora might display inaccurate info so double check its response. 
+                </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
---
+++ src/context/Context.jsx
@@ -0,0 +1,78 @@
+import { createContext, useState } from "react";
+import runChat from "../config/flora";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+
+    const [input,setInput] = useState("");
+    const [recentPrompt,setRecentPrompt] = useState("");
+    const [prevPrompts,setPrevPrompts] = useState([]);
+    const [showResult,setShowResult] = useState(false);
+    const [loading,setLoading] = useState(false);
+    const [resultData,setResultData] = useState("");
+    const [messages,setMessages] = useState([
+        {
+            text: "<p>Hello! I am <strong>Flora AI Assistant</strong>, version 0.3, created by Steven Zhang and inspired by JushBJJ. I'm here to help you learn and explore various topics in a fun and engaging way! 🦌</p><p>For more information about Flora, you can visit <a target=”_blank” href='https://floratechnology.framer.ai/'>Flora’s Website</a>.</p><hr><h2>Your Current Configuration:</h2><ul><li><strong>🎯 Depth:</strong> High School</li><li><strong>🧠 Learning Style:</strong> Active</li><li><strong>🗣️ Communication Style:</strong> Socratic</li><li><strong>🌟 Tone Style:</strong> Encouraging</li><li><strong>🔎 Reasoning Framework:</strong> Causal</li><li><strong>😀 Emojis:</strong> ✅</li><li><strong>🌐 Language:</strong> English</li><li><strong>📚 Knowledge:</strong> None</li><li><strong>⌚ Timeframe:</strong> Disabled</li></ul><p><strong>❗Flora requires GPT-4 or Gemini with Code Interpreter to run properly❗</strong><br>It's recommended that you get <strong>ChatGPT Plus</strong> to run Flora smoothly. Sorry for the inconvenience 🙂</p><hr><p><strong>➡️ Please read the guide to configurations here:</strong> <a target=”_blank” href='https://floratechnology.framer.ai/projects/flora-ai/flora-configuration'>Here</a>. ⬅️</p><p>To start planning your learning journey, you can use the <strong>/plan</strong> command. If you're ready to dive into a lesson, just type <strong>/start</strong>! And if you need any help or want to adjust your learning preferences, let me know! 😊</p>",
+            isBot: true
+        }
+    ]);
+
+    const delayPara = (index,nextWord) => {
+        setTimeout(function () {
+            setResultData(prev=>prev+nextWord);
+        },50*index)
+    }
+
+    const loader = '<div className="loader"><hr /><hr /><hr /></div>)'
+
+    const onSent = async () => {
+        const text = input;
+        setResultData("")
+        setShowResult(true)
+        // setRecentPrompt(input)
+        setInput("")
+        setMessages([
+            ...messages,
+            {text, isBot: false},
+            {text: "", isBot: true}
+        ]);
+        setLoading(true)
+        const response = await runChat(text)
+        let newResponseArray = response.split(" ");
+        for(let i=0; i<newResponseArray.length; i++) {
+            const nextWord = newResponseArray[i];
+            delayPara(i, nextWord+" ")
+        }
+        setLoading(false)
+        
+        setMessages([
+            ...messages,
+            {text, isBot: false},
+            {text: response, isBot: true}
+        ]);
+    }
+
+    const contextValue = {
+        prevPrompts,
+        setPrevPrompts,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        showResult,
+        loading,
+        resultData,
+        input,
+        setInput,
+        messages
+    }
+
+    return (
+        <Context.Provider value={contextValue}>
+            {props.children}
+        </Context.Provider>
+    )
+
+}
+
+export default ContextProvider
